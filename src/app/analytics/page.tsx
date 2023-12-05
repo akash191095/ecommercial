d841e726252bb6cd30bded3b343e59f4175bd62b
Akash Agarwal <akash191095@gmail.com>
2023-12-05 20:08:05 +0530

    chore: update placeholder sections

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import { Box, Typography } from "@mui/material";
+
+import Image from "next/image";
 import React from "react";
 
 export default function Analytics() {
-  return <div>Analytics</div>;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      pt={5}
+    >
+      <Typography variant="h5">Cute dog of the day: </Typography>
+      <Box mt={3}>
+        <img src="https://placedog.net/500/499?random" alt="puppy" />
+      </Box>
+    </Box>
+  );
 }
